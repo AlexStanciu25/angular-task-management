8bed49058097016155281a7ad7e31abbcf08d3d1
AlexSta <stanciualexandru20@gmail.com>
2025-04-07 11:56:12 +0300

    initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, computed, Input, input } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required: true}) avatar!: string; // decorator aproach 
+  @Input({required: true}) name!: string;
+  // inputs as signals
+  //avatarSignal = input.required<string>(); // type of value that is received by input
+  //nameSignal = input.required<string>();
+
+
+  //selectedUser = DUMMY_USERS[randomIndex]; -- state change 
+  // using signal
+  //selectedUser = signal(DUMMY_USERS[randomIndex]); -- signal
+  //imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar) -- signal
+
+  get imagePath() {
+     return 'assets/users/' + this.avatar;
+  }
+  
+
+  onSelectUser() {
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) -- signal
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]) -- signal
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
+}
