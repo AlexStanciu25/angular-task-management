6caf309b205b23a985531fd75ab7845db47ce316
AlexSta <stanciualexandru20@gmail.com>
2025-04-07 13:40:15 +0300

    task-component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component,EventEmitter, Input, Output, output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -9,8 +9,13 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string; // decorator aproach 
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>(); // e important sa specificam tipul de data altfel putem emite orice tip de valoare/data
+
+  // output as function
+  //selectOutput = output<string>();
   // inputs as signals
   //avatarSignal = input.required<string>(); // type of value that is received by input
   //nameSignal = input.required<string>();
@@ -30,6 +35,7 @@ export class UserComponent {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) -- signal
     // this.selectedUser.set(DUMMY_USERS[randomIndex]) -- signal
     //this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.id);
   }
 
 }
